1631e88cdb458b16aa7a745681ca56578bba0d19
nahumsa <nahumsaa@gmail.com>
2020-11-16 11:56:46 -0300

    Ground state working, working on excited energies

--- VQD.py
+++ VQD.py
@@ -1,10 +1,26 @@
+####################
+## Helper imports ##
+####################
 import numpy as np
+
+####################
+## Qiskit imports ##
+####################
 import qiskit
 from qiskit.aqua.operators import OperatorBase, ListOp, PrimitiveOp, PauliOp
 from qiskit.aqua.operators import I, X, Y, Z
-from utils import classical_solver
 from qiskit.circuit.library import TwoLocal
+
+###################
+## Local imports ##
+###################
+from utils import classical_solver
 from Observables import *
+from SWAPTest import measure_swap_test
+
+################
+## begin code ##
+################
 
 # hamiltonian = 1/2*((I^Z) + (Z^I) + 2*( (X^X) + (Y^Y)))
 hamiltonian = 1/2*((Z^I) + (Z^Z))
@@ -16,25 +32,54 @@ class VQD(object):
     """Variational Quantum Deflation algorithm
 
     """
-    def __init__(self, n_qubits: int, 
+    def __init__(self, n_qubits: int, n_excited_states: int, beta: float,
                  optimizer: qiskit.aqua.components.optimizers.Optimizer, 
-                 backend: qiskit.providers.BaseBackend):
+                 backend: qiskit.providers.BaseBackend,
+                 num_shots: int=10000):
         """Initialize the class.
 
         Args:
             n_qubits (int): Number of qubits to use with the hardware efficient ansatz.
-            optimizer (qiskit.aqua.components.optimizers.Optimizer): 
-            Classical Optimizers from aqua components.
+            n_excited_states (int): Number of excited states that you want to find the energy.
+            beta (float): Strenght parameter for the swap test.
+            optimizer (qiskit.aqua.components.optimizers.Optimizer): Classical Optimizers 
+            from aqua components.
             backend (qiskit.providers.BaseBackend): Backend for running the algorithm.
+            num_shots (int): Number of shots. (Default: 10000)
         """
-
-        self.n_qubits = n_qubits
-        self.ansatz = self._get_ansatz(n_qubits)
+        
+        # Input parameters
+        self.n_qubits = n_qubits        
         self.optimizer = optimizer
         self.backend = backend
-        self.states = []
-        self.energies = []
-        self.NUM_SHOTS = 10000
+        self.NUM_SHOTS = num_shots
+        self.BETA = beta
+        
+        # Helper Parameters
+        self.n_excited_states = n_excited_states
+        self.n_parameters = self._get_num_parameters(self.n_qubits)
+        self.ansatz = self._get_ansatz(n_qubits)
+        # Logs
+        self._states = []
+        self._energies = []
+
+    @property
+    def energies(self):
+        """ Returns a list with energies.
+
+        Returns:
+            list: list with energies
+        """
+        return self._energies
+    
+    @property
+    def states(self):
+        """ Returns a list with states associated with each energy.
+
+        Returns:
+            list: list with states.
+        """
+        return self._states
 
     def _get_ansatz(self, n_qubits):
         return TwoLocal(n_qubits, ['ry','rz'], 'cx', reps=1)
@@ -89,17 +134,33 @@ class VQD(object):
         # Hamiltonian
         hamiltonian = self.get_hamiltonian(qc, self.backend, self.NUM_SHOTS)
         
+        # Fidelity
+        fidelity = 0.
+        if len(self.states) != 0:
+            for state in self.states:
+                fidelity += measure_swap_test(qc, state, self.backend, self.NUM_SHOTS)
+
         # Get the cost function
-        cost = hamiltonian
-        print(cost)
+        cost = hamiltonian + self.BETA*fidelity
+        
         return cost
 
     def optimizer_run(self):
-        n_parameters = self._get_num_parameters(self.n_qubits)
 
-        params = np.random.rand(n_parameters)
+        params = np.random.rand(self.n_parameters)
 
-        optimal_params, energy, n_iters = self.optimizer.optimize(num_vars=n_parameters, 
+        optimal_params, energy, n_iters = self.optimizer.optimize(num_vars=self.n_parameters, 
                                                                     objective_function=self.energy_evaluation, 
                                                                     initial_point=params)
-        print(energy)
+        
+        # Logging the energies and states
+        # TODO: Change to an ordered list.
+        
+        self._energies.append(energy)
+        self._states.append(self._get_varform_params(optimal_params))
+    
+    
+
+##############        
+## End code ##
+##############
