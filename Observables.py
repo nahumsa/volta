157ed48db04a27cd65cc5192ee6b5cf0ef440d5d
nahumsa <nahumsaa@gmail.com>
2020-11-16 18:08:17 -0300

    Added aqua operator support

--- Observables.py
+++ Observables.py
@@ -1,6 +1,28 @@
-from qiskit import Aer, execute
+from typing import Union
 import qiskit
+import numpy as np
+from qiskit import Aer, execute
+from qiskit.aqua.utils.backend_utils import is_aer_provider
+from qiskit.aqua.operators import (PauliExpectation, CircuitSampler, ExpectationFactory,
+                                   CircuitStateFn, StateFn, ListOp)
 
+def sample_hamiltonian(hamiltonian: qiskit.aqua.operators.OperatorBase,
+                       backend: Union[qiskit.providers.BaseBackend, qiskit.aqua.QuantumInstance],
+                       ansatz: qiskit.QuantumCircuit):
+    
+    sampler = CircuitSampler(backend)
+    expectation = ExpectationFactory.build(operator=hamiltonian,backend=backend)
+    observable_meas = expectation.convert(StateFn(hamiltonian, is_measurement=True))
+    
+    ansatz_circuit_op = CircuitStateFn(ansatz)
+    
+    expect_op = observable_meas.compose(ansatz_circuit_op).reduce()
+    sampled_expect_op = sampler.convert(expect_op)
+    
+    return np.real(sampled_expect_op.eval())
+
+
+# Changed for the more general function sample_hamiltonian
 def _measure_zz_circuit(qc: qiskit.QuantumCircuit) -> qiskit.QuantumCircuit:
     """ Construct the circuit to measure 
     """
