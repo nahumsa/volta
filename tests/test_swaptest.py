e0f683113d4b7b0fd12c78db37befbaa149c8d2c
nahumsa <nahumsaa@gmail.com>
2021-08-31 13:55:47 -0300

    add amplitude transition test

--- tests/test_swaptest.py
+++ tests/test_swaptest.py
@@ -10,7 +10,11 @@
 import unittest
 from qiskit import QuantumCircuit, BasicAer
 from qiskit.utils import QuantumInstance
-from volta.swaptest import measure_swap_test, measure_dswap_test
+from volta.swaptest import (
+    measure_swap_test,
+    measure_dswap_test,
+    measure_amplitude_transition_test,
+)
 
 
 class TestSWAPTest(unittest.TestCase):
@@ -86,6 +90,44 @@ class TestDestructiveSWAPTest(unittest.TestCase):
     def test_11states(self):
         want = 1.0
         got = measure_dswap_test(self.qc1, self.qc1, self.backend)
+        decimalPlace = 2
+        message = "Swap test not working for states 1 and 1."
+        self.assertAlmostEqual(want, got, decimalPlace, message)
+
+
+class TestAmplitudeTransitionTest(unittest.TestCase):
+    def setUp(self):
+        self.qc1 = QuantumCircuit(1)
+        self.qc1.x(0)
+        self.qc2 = QuantumCircuit(1)
+        self.backend = QuantumInstance(
+            backend=BasicAer.get_backend("qasm_simulator"), shots=10000
+        )
+
+    def test_10states(self):
+        want = 0.0
+        got = measure_amplitude_transition_test(self.qc2, self.qc1, self.backend)
+        decimalPlace = 1
+        message = "Swap test not working for states 0 and 1."
+        self.assertAlmostEqual(want, got, decimalPlace, message)
+
+    def test_01states(self):
+        want = 0.0
+        got = measure_amplitude_transition_test(self.qc1, self.qc2, self.backend)
+        decimalPlace = 1
+        message = "Swap test not working for states 0 and 1."
+        self.assertAlmostEqual(want, got, decimalPlace, message)
+
+    def test_00states(self):
+        want = 1.0
+        got = measure_amplitude_transition_test(self.qc2, self.qc2, self.backend)
+        decimalPlace = 2
+        message = "Swap test not working for states 0 and 0."
+        self.assertAlmostEqual(want, got, decimalPlace, message)
+
+    def test_11states(self):
+        want = 1.0
+        got = measure_amplitude_transition_test(self.qc1, self.qc1, self.backend)
         decimalPlace = 2
         message = "Swap test not working for states 1 and 1."
         self.assertAlmostEqual(want, got, decimalPlace, message)
